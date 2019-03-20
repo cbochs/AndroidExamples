1aceec60a89971087fb7b843397c991933777cf5
Calvin B <calvin.bochulak@gmail.com>
2019-03-20 14:54:01 -0600

    Adding first 5 examples

---
+++ 05_FragmentFragmentCommunication/app/src/main/java/com/calvin/fragmentfragmentcommunication/MessageFragment.java
@@ -0,0 +1,66 @@
+package com.calvin.fragmentfragmentcommunication;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MessageFragment extends Fragment {
+
+    private EditText editText;
+    private Button button;
+
+    OnMessageSendListener messageSendListener;
+    public interface OnMessageSendListener {
+        void onMessageSend(String message);
+    }
+
+
+    public MessageFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_message, container, false);
+
+        editText = view.findViewById(R.id.txt_message);
+
+        button = view.findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = editText.getText().toString();
+                messageSendListener.onMessageSend(message);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Activity activity = (Activity) context;
+        try {
+            messageSendListener = (OnMessageSendListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnMessageSendListener");
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        editText.setText("");
+    }
+}
